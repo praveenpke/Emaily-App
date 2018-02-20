aca79345fef5b51e9dedccde780d824d0ba00c81
praveen emani <praveenemani@praveens-MacBook-Pro.local>
2018-02-20 02:02:31 -0600

    made communication between client and server using proxy in package.json

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+
+        
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <a href="/auth/google">Sign In with Google </a>
+
       </div>
     );
   }
